77d7ec79d30dfa8793d607cfb259913cc221619f
Kenny Sheftel <kennysheftel@gmail.com>
2025-05-18 13:10:37 -0700

    simple react-pdf next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config: {
+    resolve: { alias: { canvas: boolean; encoding: boolean } };
+  }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
